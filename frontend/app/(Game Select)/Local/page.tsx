89cde9d7ab2d344ba95d0cc04bfbc257ac4a75f4
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-04-21 16:39:19 -0700

    Fixed hosting game. Added cookie checks to HostGame Security - Fixed vulnerability where users could create multiple lobbys and guests.

--- frontend/app/(Game Select)/Local/page.tsx
+++ frontend/app/(Game Select)/Local/page.tsx
@@ -1,26 +1,15 @@
 import Link from 'next/link';
 import HostGame from './HostGame';
 import JoinGame from './JoinGame';
-import { pb } from '@/app/api/pocketbase';
+import { cookies } from 'next/headers';
 import PocketBase from 'pocketbase';
 
 export default async function LocalLobby(){
 
     const pb = new PocketBase('http://http://localhost:8091')
-
+    const cookieStore = cookies();
+    const token = cookieStore.get('token');
     
-
-    try{
-        const cookie = await fetch('http://localhost:3000/api/Local');
-        if(cookie.ok){
-            console.log(cookie);
-        }else{
-            console.log("warning")
-        }
-    }catch (e){
-        console.log(e)
-    }
-    console.log(pb.authStore.loadFromCookie('token'));
 
     return (
         <div className="min-h-screen">
@@ -28,7 +17,7 @@ export default async function LocalLobby(){
             <div className='hero min-h-screen'>
                 <div className="hero-content text-center text-primary flex flex-col gap-5">
                     <JoinGame/>
-                    <HostGame/>
+                    <HostGame localToken={token?.value}/>
                 </div>
             </div>
         </div>
