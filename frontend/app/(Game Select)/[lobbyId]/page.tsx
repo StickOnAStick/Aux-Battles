cd5fbab2d384942141c70f0be73005453c43ea8c
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-04-21 17:29:18 -0700

    Begin leave LeaveLobbyModal

--- frontend/app/(Game Select)/[lobbyId]/page.tsx
+++ frontend/app/(Game Select)/[lobbyId]/page.tsx
@@ -43,7 +43,8 @@ export default async function Lobby({
         <div className="flex flex-col min-h-screen">
             <div className="flex justify-center py-2">
                 <button className="text-4xl font-bold btn btn-ghost z-10">
-                    <Link href="/Play">Aux-Battles</Link> 
+                {/* Add checks to see if user is host or guest. If Host, popup modal asking if they want to close the lobby. If Guest, ask if they want to leave lobby. If signed, ask if they want to leave or check something else?*/}
+                    <Link href="/Play">Aux-Battles</Link>  
                 </button>
             </div>      
             <div className="flex flex-col justify-center grow min-w-full z-0 lg:p-4">
