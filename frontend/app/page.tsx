47411574daeffcf971569dd1d1e678d55f614dd3
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-06-29 00:27:57 -0700

    Added beta tag

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="hero-overlay bg-opacity-0"></div>
         <div className="hero-content text-center text-primary">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Aux Battles</h1>
+            <h1 className="mb-5 text-5xl font-bold">Aux Battles <span className=" text-base bg-warning p-2 rounded-lg align-middle tracking-wide">BETA</span></h1>
             <Link href="/Play" className="btn btn-accent font-bold text-xl text-primary animate-bounce">Play</Link>
           </div>
         </div>
