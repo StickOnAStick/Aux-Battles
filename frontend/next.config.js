b08690e561998d864bebb88ef44320330272e550
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2023-07-01 20:27:07 -0700

    Fixed images and socket to run on https

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8091',
+        protocol: 'https',
+        hostname: 'aux-battles.app',
+        port: '443',
         pathname: '/api/files/**'
       },
       {
